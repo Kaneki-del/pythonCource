e99c498387af00d0e03cef9c811b9f5b3a28d3a1
Soufiane <sofiansofianait@gmail.com>
2025-07-28 16:35:25 +0000

    filting the return links to only votes that are more than 100 and sort the return values

--- scrapingData/scrap.py
+++ scrapingData/scrap.py
@@ -2,7 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 url = 'https://news.ycombinator.com/news'
 res = requests.get(url)
-if res.status_code == 200:
-    print(res.text)
-else: 
-    print('somting went wrong')
+
+
+# def get_value(dic):
+#     return dic['point']
+
+def get_info(links, subtext):
+    hr = []
+    for index, it in enumerate(links):
+        title = it.getText()
+        herf = it.find('a').get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace("points", ""))
+            if (points > 99):
+                hr.append({'title': title, 'link': herf, 'point':points})
+    hr.sort(reverse=True, key = lambda dic: dic['point'])
+    return hr
+def main():
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, "html.parser")
+        links =   soup.select('.titleline')
+        subtext =  soup.select('.subtext')
+        print(get_info(links, subtext))
+    else: 
+        print('somting went wrong')
+
+
+main()
