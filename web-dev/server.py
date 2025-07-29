cb4ea0cbd550d56186f3bd8f208865e1f03823c4
Soufiane Ait naceur <sait-nac@e3r11p8.1337.ma>
2025-07-29 19:12:42 +0100

    start the form taking

--- web-dev/server.py
+++ web-dev/server.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
+
+@app.route("/submit_form", methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+    return redirect('/tanks.html')
+
